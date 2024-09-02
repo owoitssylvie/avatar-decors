f276ac87e2fc919cf3b8d61d2ab0106af93ffcc5
Gabe-W-J <gabriel.winston.jackson@gmail.com>
2024-09-02 17:04:48 +0200

    small font change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 h-5 w-full max-w-1.5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 h-5 w-full max-w-1.5xl items-center justify-between text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Jelly's Space - &nbsp;
-          <code className="font-mono font-bold">Avatar Decors</code>
+          <code className=" font-bold">Avatar Decors</code>
         </p>
         <div className="fixed left-0 flex w-full justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <Image
